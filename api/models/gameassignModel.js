1dd4779729f39414a5efd7ee926026c5fb541e18
virtualninjach <chris@lwaldtech.com>
2017-09-13 18:22:37 -0400

    added express functionality

--- api/models/gameassignModel.js
+++ api/models/gameassignModel.js
@@ -4,54 +4,54 @@ var Schema = mongoose.Schema;
 
 
 var gameSchema = new Schema({
-  Game: {
-    Date: Date,
-    Time: String
-  },
-  Location: {
-    Address1: String,
-    Address2: String,
-    Coord: String,
-    StateProvidence: String,
-    ZipCode:String,
-    Country:String
-  },
-  League: {
-    Name: String,
-    Divsion: String,
-    ContactName1: String,
-    ContactNumber1: String,
-    Fee:String 
-},
-  RefName1: {
-        FirstName: String,
-        LastName: String,
-        Pos: String,
-        LicenseNumber:String,
-        GradeNumber: String,
-        AcceptanceStatus:String
-  },
-  RefName2: {
-    FirstName: String,
-    LastName: String,
-    Pos: String,
-    LicenseNumber:String,
-    GradeNumber: String,
-    AcceptanceStatus:String
-},
-RefName3: {
-  FirstName: String,
-  LastName: String,
-  Pos: String,
-  LicenseNumber:String,
-  GradeNumber: String,
-  AcceptanceStatus:String
-},
-Created_date: {
-  type: Date,
-  default: Date.now()
-},
-}
+    Game: {
+      Date: Date,
+      Time: String
+    },
+    Location: {
+      Address1: String,
+      Address2: String,
+      Coord: String,
+      StateProvidence: String,
+      ZipCode:String,
+      Country:String
+    },
+    League: {
+      Name: String,
+      Divsion: String,
+      ContactName1: String,
+      ContactNumber1: String,
+      Fee:String 
+    },
+    RefName1: {
+      FirstName: String,
+      LastName: String,
+      Pos: String,
+      LicenseNumber:String,
+      GradeNumber: String,
+      AcceptanceStatus:String
+    },
+    RefName2: {
+      FirstName: String,
+      LastName: String,
+      Pos: String,
+      LicenseNumber:String,
+      GradeNumber: String,
+      AcceptanceStatus:String
+    },
+    RefName3: {
+      FirstName: String,
+      LastName: String,
+      Pos: String,
+      LicenseNumber:String,
+      GradeNumber: String,
+      AcceptanceStatus:String
+    },
+    Created_date: {
+      type: Date,
+      default: Date.now()
+    },
+  }
 );
 
 module.exports = mongoose.model('Games', gameSchema);
