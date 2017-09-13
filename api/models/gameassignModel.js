e4bf579b1e41def78c7bc322328bbef47a513505
virtualninjach <chris@lwaldtech.com>
2017-09-13 14:52:06 -0400

    updated game model

--- api/models/gameassignModel.js
+++ api/models/gameassignModel.js
@@ -11,13 +11,14 @@ var gameSchema = new Schema({
   Location: {
     Address1: String,
     Address2: String,
-    Coor: Coordinates,
+    Coord: String,
     StateProvidence: String,
     ZipCode:String,
     Country:String
   },
   League: {
     Name: String,
+    Divsion: String,
     ContactName1: String,
     ContactNumber1: String,
     Fee:String 
@@ -27,12 +28,30 @@ var gameSchema = new Schema({
         LastName: String,
         Pos: String,
         LicenseNumber:String,
-        GradeNumber: String
+        GradeNumber: String,
+        AcceptanceStatus:String
   },
-  Created_date:Date.now,
-
-  GameAcceptStatus:String
-  }
+  RefName2: {
+    FirstName: String,
+    LastName: String,
+    Pos: String,
+    LicenseNumber:String,
+    GradeNumber: String,
+    AcceptanceStatus:String
+},
+RefName3: {
+  FirstName: String,
+  LastName: String,
+  Pos: String,
+  LicenseNumber:String,
+  GradeNumber: String,
+  AcceptanceStatus:String
+},
+Created_date: {
+  type: Date,
+  default: Date.now()
+},
+}
 );
 
 module.exports = mongoose.model('Games', gameSchema);
