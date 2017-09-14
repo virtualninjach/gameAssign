cdfe849cae9b7898a007150c2ee1e82c0b1354c6
virtualninjach <chris@lwaldtech.com>
2017-09-13 20:36:47 -0400

    added express functionality

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'gameAssign Test Page!' });
+});
+
+/* GET Userlist page. */
+router.get('/gamelist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('games');
+  collection.find({},{},function(e,docs){
+      res.render('gamelist', {
+          "gamelist" : docs
+      });
+  });
+});
+
+module.exports = router;
